e69b4a1b01966f928687a0135361fd8ee4a7ad55
HacDEVer <136824612+nilton2005@users.noreply.github.com>
2024-05-08 22:04:54 -0500

    Update BinaryTreeNode.py

--- Program_Menu/BinaryTreeNode.py
+++ Program_Menu/BinaryTreeNode.py
@@ -1,8 +1,5 @@
-'''
-Created on Mar 11, 2019
-@author: jgomezm
-'''
-class BinaryTreeNode:
+
+class BinaryTreeNode:            
 
     def __init__(self, data, left = None, right = None):
         self.data = data  # value node
@@ -10,18 +7,18 @@ class BinaryTreeNode:
         self.right = right # right child
 
     # Method for setting the data
-    def setData(self, data):
-        self.data = data
+    def setData(self, data):            
+        self.data = data            
 
     # Method for getting the data
-    def getData(self):
-        return self.data
+    def getData(self):            
+        return self.data            
 
     # Get left child of a node
-    def getLeft(self):
-        return self.left
+    def getLeft(self):            
+        return self.left            
 
     # Get right child of a node
-    def getRight(self):
-        return self.right
+    def getRight(self):            
+        return self.right            
 
